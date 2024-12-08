f677d1b384081d0b88e90a13fd7015f9116a88c9
MdAfsarHossain <afsar291722@gmail.com>
2024-12-08 12:50:01 +0600

    Private Route jsx file added

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+import { Navigate, useLocation } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+
+  if (loading) {
+    return (
+      <div className="flex flex-col justify-center items-center mt-32 mb-96">
+        <span className="loading loading-spinner loading-lg bg-success"></span>
+      </div>
+    );
+  }
+
+  if (user) {
+    return children;
+  }
+
+  return <Navigate state={location.pathname} to="/login"></Navigate>;
+};
+
+export default PrivateRoute;
+
+PrivateRoute.propTypes = {
+  children: PropTypes.node,
+};
