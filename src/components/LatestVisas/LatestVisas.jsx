d4fb8ea26ba6c8c0fb15f3422897b196c16c3036
MdAfsarHossain <afsar291722@gmail.com>
2024-12-08 13:00:28 +0600

    Latest Visas Component jsx file added

---
+++ src/components/LatestVisas/LatestVisas.jsx
@@ -0,0 +1,63 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Typewriter } from "react-simple-typewriter";
+import SingleVisaCard from "../SingleVisaCard/SingleVisaCard";
+
+const LatestVisas = () => {
+  const [latestVisasData, setLatestVisasData] = useState([]);
+
+  // Fetch latest visas data from an API or a database
+  useEffect(() => {
+    const getLatestVisasData = async () => {
+      const { data } = await axios.get(
+        `${import.meta.env.VITE_API_URL}/latest-visas`
+      );
+
+      setLatestVisasData(data);
+    };
+
+    getLatestVisasData();
+  }, []);
+
+  return (
+    <div className="mb-40">
+      {/* Heading */}
+      <div className="mt-20">
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold uppercase text-center">
+          Latest{" "}
+          <span className="text-green-600">
+            <Typewriter
+              words={["Visas"]}
+              loop={0}
+              cursor
+              cursorStyle="_"
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+          </span>{" "}
+          {/* Data */}
+        </h1>
+      </div>
+
+      {/* Latest Data */}
+      <div className="px-10 md:px-10 lg:px-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 lg:gap-5 mt-14">
+        {latestVisasData?.map((visa) => (
+          <SingleVisaCard key={visa?._id} visa={visa}></SingleVisaCard>
+        ))}
+      </div>
+
+      <div className="flex flex-col justify-center items-center mt-10">
+        <Link
+          to="/all-visas"
+          className="bg-white text-green-500  font-bold px-8 py-2 rounded-full border-2 border-green-500 hover:text-white hover:bg-green-500 hover:border-green-500 transition-all uppercase"
+        >
+          See All Visas
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default LatestVisas;
