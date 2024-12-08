a0068dfbdcf6fe4e6084b2b6279383c85035ba26
MdAfsarHossain <afsar291722@gmail.com>
2024-12-08 22:25:58 +0600

    Loading Spinner added into the My Visa Applications JSX file

--- src/pages/MyVisaApplications/MyVisaApplications.jsx
+++ src/pages/MyVisaApplications/MyVisaApplications.jsx
@@ -11,6 +11,7 @@ const MyVisaApplications = () => {
   const { user } = useAuth();
   const [myVisaApplications, setMyVisaApplications] = useState([]);
   const [searchText, setSearchText] = useState("");
+  const [flag, setFlag] = useState(true);
 
   useEffect(() => {
     const getAllVisaApplicationsData = async () => {
@@ -19,28 +20,30 @@ const MyVisaApplications = () => {
       //     user?.email
       //   }&search=${searchText}`
       // );
+      setFlag(true);
       const { data } = await axios.get(
         `${import.meta.env.VITE_API_URL}/my-visa-applications?applicantEmail=${
           user?.email
         }`
       );
       setMyVisaApplications(data);
+      setFlag(false);
     };
 
     window.scrollTo({ top: 0, behavior: "smooth" });
 
     getAllVisaApplicationsData();
+    window.scrollTo({ top: 0, behavior: "smooth" });
   }, []);
   // searchText
 
-  useEffect(() => {
-    setTimeout(() => {
-      document.getElementById("loadingSpinner").style.display = "none";
-      document.getElementById("handleLoading").style.display = "block";
-    }, 2000);
+  // useEffect(() => {
+  // setTimeout(() => {
+  // document.getElementById("loadingSpinner").style.display = "none";
+  // document.getElementById("handleLoading").style.display = "block";
+  // }, 2000);
 
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }, []);
+  // }, []);
 
   // Hanlde Delete Btn clicked
   const handleDelete = async (id) => {
@@ -107,22 +110,24 @@ const MyVisaApplications = () => {
   const handleSearchBtn = async () => {
     // console.log("Search btn click");
 
-    document.getElementById("visasAllDataId").style.display = "none";
-    document.getElementById("loadingSpinnerForSearchData").style.display =
-      "flex";
+    // document.getElementById("visasAllDataId").style.display = "none";
+    // document.getElementById("loadingSpinnerForSearchData").style.display =
+    // "flex";
 
-    setTimeout(() => {
-      document.getElementById("loadingSpinnerForSearchData").style.display =
-        "none";
-      document.getElementById("visasAllDataId").style.display = "grid";
-    }, 2000);
+    // setTimeout(() => {
+    // document.getElementById("loadingSpinnerForSearchData").style.display =
+    // "none";
+    // document.getElementById("visasAllDataId").style.display = "grid";
+    // }, 2000);
 
+    setFlag(true);
     const { data } = await axios.get(
       `${import.meta.env.VITE_API_URL}/my-visa-applications?applicantEmail=${
         user?.email
       }&search=${searchText}`
     );
     setMyVisaApplications(data);
+    setFlag(false);
   };
 
   return (
@@ -132,17 +137,8 @@ const MyVisaApplications = () => {
         <title>World Pass Express | My Visa Applications</title>
       </Helmet>
 
-      {/* Loading Spinner */}
-
-      <div
-        id="loadingSpinner"
-        className="h-screen flex flex-col justify-center items-center"
-      >
-        <span className="-mt-28 flex flex-row justify-center items-center h-56 mx-auto loading loading-spinner loading-lg text-success"></span>
-      </div>
-
       {/* Remove Hidden Class */}
-      <div id="handleLoading" className="hidden">
+      <div id="handleLoading" className="">
         {/* Heading */}
         <div className="mt-10">
           <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold uppercase text-center">
@@ -186,36 +182,54 @@ const MyVisaApplications = () => {
           </div>
         </div>
 
-        {myVisaApplications?.length === 0 && (
-          <div className="flex flex-col justify-center items-center mt-10">
-            <img className="w-40 h-40" src="/error.png" alt="" />
-            <h1 className="text-3xl font-bold uppercase text-red-500">
-              No Data founds
-            </h1>
+        {/* Loading Spinner */}
+        {flag && (
+          <div
+            id="loadingSpinner"
+            className="h-28 py-52  flex flex-col justify-center items-center"
+          >
+            <span className="-mt-28 flex flex-row justify-center items-center h-56 mx-auto loading loading-spinner loading-lg text-success"></span>
           </div>
         )}
 
-        <div
+        {!flag && (
+          <>
+            {myVisaApplications?.length === 0 && (
+              <div className="flex flex-col justify-center items-center mt-10">
+                <img className="w-40 h-40" src="/error.png" alt="" />
+                <h1 className="text-3xl font-bold uppercase text-red-500">
+                  No Data founds
+                </h1>
+              </div>
+            )}
+          </>
+        )}
+
+        {/* <div
           id="loadingSpinnerForSearchData"
           className="h-28 flex-col justify-center items-center hidden"
         >
           <span className="flex flex-row justify-center items-center h-28 mx-auto loading loading-spinner loading-lg text-success"></span>
-        </div>
+        </div> */}
 
         {/* hidden */}
-        {myVisaApplications?.length !== 0 && (
-          <div id="visasAllDataId" className="">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 lg:gap-5 rounded-xl mt-10">
-              {myVisaApplications?.map((myApplication, idx) => (
-                <MyVisaApplicationCard
-                  key={idx}
-                  myApplication={myApplication}
-                  visaId={myApplication?.visaId}
-                  handleDelete={handleDelete}
-                ></MyVisaApplicationCard>
-              ))}
-            </div>
-          </div>
+        {!flag && (
+          <>
+            {myVisaApplications?.length !== 0 && (
+              <div id="visasAllDataId" className="">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 lg:gap-5 rounded-xl mt-10">
+                  {myVisaApplications?.map((myApplication, idx) => (
+                    <MyVisaApplicationCard
+                      key={idx}
+                      myApplication={myApplication}
+                      visaId={myApplication?.visaId}
+                      handleDelete={handleDelete}
+                    ></MyVisaApplicationCard>
+                  ))}
+                </div>
+              </div>
+            )}
+          </>
         )}
       </div>
     </div>
