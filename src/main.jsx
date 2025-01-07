db9a11087afbc7e72e8a26e7592065295f8e9bdf
MdAfsarHossain <afsar291722@gmail.com>
2025-01-07 14:14:09 +0600

    Main jsx file updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,28 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
-import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import AuthProvider from "./providers/AuthProvider.jsx";
 import router from "./routes/Router.jsx";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { HelmetProvider } from "react-helmet-async";
+import { Toaster } from "react-hot-toast";
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
     <AuthProvider>
-      <HelmetProvider>
-        <RouterProvider router={router} />
-        <Toaster />
-      </HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <HelmetProvider>
+          <RouterProvider router={router} />
+          <Toaster />
+        </HelmetProvider>
+      </QueryClientProvider>
     </AuthProvider>
   </StrictMode>
 );
